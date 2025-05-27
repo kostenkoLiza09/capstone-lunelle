593bf1182693528737867ef0e0be5830a0e70348
kostenkoLiza09 <kostaz063@gmail.com>
2025-05-27 21:32:22 +0200

    add PerfumePlpDto and test

---
+++ backend/src/main/java/org/example/backend/model/plp/PerfumePlpDto.java
@@ -0,0 +1,27 @@
+package org.example.backend.model.plp;
+
+import org.example.backend.model.enums.Volume;
+import org.example.backend.model.record.Perfume;
+import org.example.backend.model.record.PerfumeVariant;
+
+public record PerfumePlpDto(
+        String name,
+        String imageURL,
+        float price,
+        Volume volume
+) {
+    public PerfumePlpDto(Perfume perfume) {
+        this(
+                perfume.name(),
+                perfume.imageURL(),
+                perfume.variants().stream()
+                        .findFirst()
+                        .map(PerfumeVariant::price)
+                        .orElse(0f),
+                perfume.variants().stream()
+                        .findFirst()
+                        .map(PerfumeVariant::volume)
+                        .orElse(null)
+        );
+    }
+}
