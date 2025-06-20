98dc8dc67dab7f5dd54a91a47b1078793ce40a69
kostenkoLiza09 <kostaz063@gmail.com>
2025-06-20 15:09:31 +0200

    fix frontend

--- frontend/src/components/banner/Banner.tsx
+++ frontend/src/components/banner/Banner.tsx
@@ -5,34 +5,29 @@ import Banner3 from '../../assets/images/banner/Banner3.jpg';
 import Banner4 from '../../assets/images/banner/Banner4.jpg';
 import Banner5 from '../../assets/images/banner/Banner5.jpg';
 import Banner6 from '../../assets/images/banner/Banner6.jpg';
+import { useEffect, useState } from "react";
 import { useLocation } from 'react-router-dom';
-import {useEffect, useState} from "react";
 
-const banners = [Banner1, Banner2, Banner3, Banner4, Banner5, Banner6];
+const banners: string[] = [Banner1, Banner2, Banner3, Banner4, Banner5, Banner6];
 
 const Banner = () => {
-
     const location = useLocation();
-    const [current, setCurrent] = useState(Banner1);
+    const [currentIndex, setCurrentIndex] = useState(0);
 
     useEffect(() => {
-        const randomIndex = Math.floor(Math.random() * banners.length);
-        setCurrent(banners[randomIndex]);
+        setCurrentIndex(prev => (prev + 1) % banners.length);
     }, [location.pathname]);
-
-
 
     return (
         <div className="banner">
             <div className="left">
-                <img src={current} alt="Banner" className="image" />
+                <img src={banners[currentIndex]} alt="Banner" className="image" />
             </div>
             <div className="right">
                 <h1 className="title">ELEVATE YOUR SENSES</h1>
                 <p className="description">
                     Indulge in our exclusive selection of perfumes — where every note tells a story of elegance, passion, and individuality.
                 </p>
-
             </div>
         </div>
     );
