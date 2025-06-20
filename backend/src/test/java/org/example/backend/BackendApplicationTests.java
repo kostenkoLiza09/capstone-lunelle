5182071bf6d65c71e6604dbdb564c22a6e49d44b
kostenkoLiza09 <kostaz063@gmail.com>
2025-06-20 17:03:49 +0200

    fix backend

--- backend/src/test/java/org/example/backend/BackendApplicationTests.java
+++ backend/src/test/java/org/example/backend/BackendApplicationTests.java
@@ -1,13 +1,20 @@
 package org.example.backend;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.autoconfigure.ImportAutoConfiguration;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityFilterAutoConfiguration;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
 
 @ActiveProfiles("test")
 @SpringBootTest
-@AutoConfigureMockMvc(addFilters = false)
+@ImportAutoConfiguration(exclude = {
+		SecurityAutoConfiguration.class,
+		SecurityFilterAutoConfiguration.class
+})
+
 class BackendApplicationTests {
 
 	@Test
--- backend/src/test/java/org/example/backend/security/AuthControllerTest.java
+++ backend/src/test/java/org/example/backend/security/AuthControllerTest.java
@@ -9,8 +9,6 @@ import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
-import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.oidcLogin;
-
 @SpringBootTest
 @AutoConfigureMockMvc
 @WithMockUser(username = "testUser")
